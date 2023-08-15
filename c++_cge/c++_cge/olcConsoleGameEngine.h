98f429820178a37f0e4887184d51288ff9adb8de
ornfelt <jonasornfelt@outlook.com>
2023-08-16 00:15:54 +0200

    Update cge / pge code

--- c++_cge/c++_cge/olcConsoleGameEngine.h
+++ c++_cge/c++_cge/olcConsoleGameEngine.h
@@ -1409,4 +1409,3 @@ protected:
 std::atomic<bool> olcConsoleGameEngine::m_bAtomActive(false);
 std::condition_variable olcConsoleGameEngine::m_cvGameFinished;
 std::mutex olcConsoleGameEngine::m_muxGame;
-
